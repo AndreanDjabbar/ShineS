2da54ffd57ceb1d456bc3b3a77989815e65363b0
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-04-03 10:47:23 +0700

    Restruktur folder project, membuat middlewares untuk session, membuat login page dan register page, dan membuat auth router

---
+++ middlewares/session.go
@@ -0,0 +1,65 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+)
+
+const userKey = "User"
+
+func AuthSession() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session := sessions.Default(c)
+		userSession := session.Get(userKey)
+		if userSession == nil {
+			c.JSON(
+				http.StatusUnauthorized,
+				gin.H{"Error":"Authentication Failed"},
+			)
+			return
+		} else {
+			c.Next()
+		}
+	}
+}
+
+func SetSession() gin.HandlerFunc {
+	store := cookie.NewStore([]byte(userKey))
+	return sessions.Sessions("mySession", store)
+}
+
+func SaveSession(c *gin.Context, username string) {
+	session := sessions.Default(c)
+	session.Set(userKey, username)
+	session.Save()
+}
+
+func GetSession(c *gin.Context) string {
+	session := sessions.Default(c)
+	userSession := session.Get(userKey)
+	if userSession == nil {
+		return "Anonymous"
+	} else {
+		return userSession.(string)
+	}
+}
+
+func ClearSession(c *gin.Context) {
+	session := sessions.Default(c)
+	session.Delete(userKey)
+	session.Clear()
+	session.Save()
+}
+
+func CheckSession(c *gin.Context) bool {
+	session := sessions.Default(c)
+	userSession := session.Get(userKey)
+	if userSession == nil {
+		return false
+	} else {
+		return true
+	}
+}
