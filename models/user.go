daf733209954281d4b1554444acab2042362a96b
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-04-05 11:54:42 +0700

    Membuat model user di user.go

--- models/user.go
+++ models/user.go
@@ -1,1 +1,18 @@
 package models
+
+type userRole string
+
+const (
+	Admin userRole = "admin"
+	Seller userRole = "seller"
+	Customer userRole = "customer"
+)
+
+type User struct {
+	UserId uint `gorm:"primaryKey"`
+	Username string `gorm:"unique;not null"`
+	Email string `gorm:"unique;not null"`
+	Phone string `gorm:"unique"`
+	Password string `gorm:"unique;not null"`
+	Role userRole `gorm:"unique"`
+}
