e9f249fa14ad7bf49432c750a5403476a8eb89d9
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2024-05-28 09:56:47 +0700

    Membuat router untuk seller catalog page

--- routers/router.go
+++ routers/router.go
@@ -28,5 +28,7 @@ func MainRouter(c *gin.RouterGroup) {
 		main.POST("create-product-page/", controllers.CreateProductHandler)
 		main.GET("update-product-page/:productId", controllers.ViewUpdateProductHandler)
 		main.POST("update-product-page/:productId", controllers.UpdateProductHandler)
+		main.GET("seller-catalog-page/", controllers.ViewSellerCatalogHandler)
+		main.POST("seller-catalog-page/", controllers.SellerCatalogHandler)
 	}
 }
